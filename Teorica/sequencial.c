ab70f31dd1f2610098e39b9df6a66a3c229f8875
Pedro Ferreira <pedroferreira.prado@sou.unifal-mg.edu.br>
2025-09-13 13:37:47 -0300

    Implement sequential search function in sequencial.c and add header file alocacao.h

---
+++ Teorica/sequencial.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "alocacao.h"
+
+int busca(int x){
+    int i = 1; int busca1=0;
+    while(i <= n){
+        if(L[i].chave = x){
+            busca1 = i;
+            i=n+1;
+        } else{
+            i++;
+        }
+    }   
+
+    return 0;
+}
